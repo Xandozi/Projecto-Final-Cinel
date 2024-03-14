6a45ff73a5df54f6b0462a4aeec95cf861954b93
Xandozi <46765984+Xandozi@users.noreply.github.com>
2024-03-14 08:47:18 +0000

    Classes for Inscrições
    Created all the classes I need for the inscrições to happen and work properly. Also added SQL Stored Procedures to work with these classes as well.

--- Classes/Formadores.cs
+++ Classes/Formadores.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,36 @@ namespace Projeto_Final.Classes
 {
     public class Formadores
     {
+        public int cod_formador { get; set; }
+        public int cod_inscricao { get; set; }
+
+        public static bool Inserir_Formador(int cod_formador, int cod_inscricao)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["CinelConnectionString"].ConnectionString);
+
+            using (SqlCommand myCommand = new SqlCommand())
+            {
+                myCommand.Parameters.AddWithValue("@cod_formador", cod_formador);
+                myCommand.Parameters.AddWithValue("@cod_inscricao", cod_inscricao);
+
+                SqlParameter valido = new SqlParameter();
+                valido.ParameterName = "@valido";
+                valido.Direction = ParameterDirection.Output;
+                valido.SqlDbType = SqlDbType.Bit;
+                myCommand.Parameters.Add(valido);
+
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.CommandText = "Insert_Formador";
+
+                myCommand.Connection = myConn;
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                bool resposta_sp = Convert.ToBoolean(myCommand.Parameters["@valido"].Value);
+
+                myConn.Close();
+
+                return resposta_sp;
+            }
+        }
     }
 }
--- Classes/Inscricoes.cs
+++ Classes/Inscricoes.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -10,8 +13,49 @@ namespace Projeto_Final.Classes
         public int cod_inscricao { get; set; }
         public int cod_user { get; set; }
         public int cod_curso { get; set; }
-        public DateTime dataa { get; set; }
+        public DateTime data_inscricao { get; set; }
         public int cod_formando { get; set; }
         public int cod_formador { get; set; }
+        public int cod_situacao { get; set; }
+        public string situacao { get; set; }
+        public DateTime data_situacao { get; set; }
+
+        public static int Insert_Inscricao(int tipo_inscricao, int cod_user, int cod_curso, DateTime data_inscricao)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["CinelConnectionString"].ConnectionString);
+
+            using (SqlCommand myCommand = new SqlCommand())
+            {
+                myCommand.Parameters.AddWithValue("@cod_user", cod_user);
+                myCommand.Parameters.AddWithValue("@cod_curso", cod_curso);
+                myCommand.Parameters.AddWithValue("@data_inscricao", data_inscricao);
+
+                SqlParameter valido = new SqlParameter();
+                valido.ParameterName = "@valido";
+                valido.Direction = ParameterDirection.Output;
+                valido.SqlDbType = SqlDbType.Int;
+                myCommand.Parameters.Add(valido);
+
+                if(tipo_inscricao == 1)
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.CommandText = "Insert_Inscricao_Formador";
+                }
+                else if(tipo_inscricao == 2)
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.CommandText = "Insert_Inscricao_Formando";
+                }
+
+                myCommand.Connection = myConn;
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                int resposta_sp = Convert.ToInt32(myCommand.Parameters["@valido"].Value);
+
+                myConn.Close();
+
+                return resposta_sp;
+            }
+        }
     }
 }
