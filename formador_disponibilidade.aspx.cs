277f1b4d54575e1dcd78063f799733711529600d
Xandozi <46765984+Xandozi@users.noreply.github.com>
2024-03-28 06:50:45 +0000

    FullCalendar Development
    Made some work on the fullcalendar for the Disponibilidade Formador. Completed the AJAX data upload to the server and also can return a string[] with the information selected on the calendar. Now I only need to work on the database insertion and logic for it to work properly

--- formador_disponibilidade.aspx.cs
+++ formador_disponibilidade.aspx.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
+using System.Web.Services;
+using System.Web.Script.Serialization;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +19,22 @@ namespace Projeto_Final
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        // Method to receive data from client-side
+        [WebMethod]
+        public static string[] ProcessSelectedSlots(string[] selectedSlots)
+        {
+            string[] slots = new string[selectedSlots.Length];
+            int i = 0;
+            // Process the selectedSlots array received from the client-side (JavaScript)
+            foreach (var slot in selectedSlots)
+            {
+                slots[i] = slot;
+                i++;
+            }
+
+            return slots;
         }
     }
 }
