5b5f7ebdc1c7921bf1c6139276deed9febe79b9a
Xandozi <46765984+Xandozi@users.noreply.github.com>
2024-04-02 09:14:50 +0100

    FullCalendar Read DB and Insert
    Made some work on reading the DB data and to insert it into the fullcalendar via events. Currently I'm only able to fetch each time_slot one by one and I need to group them up when they are contiguous so it is shown a little bit better on the calendar for the user. Currently it is all badly formatted because each day that has events on it has events with 1h each. Which makes it awful

---
+++ Classes/FullCalendarData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_Final.Classes
+{
+    public class FullCalendarData
+    {
+        public int Cod_Timeslot { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan TimeSlot_Inicio { get; set; }
+        public TimeSpan TimeSlot_Fim { get; set; }
+        public string start => $"{Data:yyyy-MM-dd}T{TimeSlot_Inicio}";
+        public string end => $"{Data:yyyy-MM-dd}T{TimeSlot_Fim}";
+    }
+}
---
+++ Disponibilidade_WebService.asmx.cs
@@ -0,0 +1,70 @@
+﻿using Projeto_Final.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Projeto_Final
+{
+    /// <summary>
+    /// Summary description for Disponibilidade_WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class Disponibilidade_WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string GetDisponibilidade_JSON(int cod_user)
+        {
+            List<FullCalendarData> lst_disponibilidade = GetDisponibilidade_DB(cod_user);
+
+            string json = JsonConvert.SerializeObject(lst_disponibilidade, Formatting.None);
+
+            return json;
+        }
+
+        public List<FullCalendarData> GetDisponibilidade_DB(int cod_user)
+        {
+            List<FullCalendarData> lst_disponibilidade = new List<FullCalendarData>();
+
+            string query = $"select Disponibilidade.cod_timeslot, Disponibilidade.dataa, Disponibilidade.cod_user, Disponibilidade.available, Timeslots.hora_inicio, Timeslots.hora_fim " +
+                           $"from Disponibilidade " +
+                           $"join Timeslots on Timeslots.cod_timeslot = Disponibilidade.cod_timeslot " +
+                           $"where Disponibilidade.cod_user = {cod_user} and Disponibilidade.available = 0";
+
+            using (SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["CinelConnectionString"].ConnectionString))
+            {
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myConn.Open();
+
+                    using (SqlDataReader dr = myCommand.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            FullCalendarData eventData = new FullCalendarData();
+                            eventData.Cod_Timeslot = !dr.IsDBNull(0) ? dr.GetInt32(0) : 000; // Set cod_timeslot
+                            eventData.Data = !dr.IsDBNull(1) ? dr.GetDateTime(1) : default(DateTime); // Set dataa
+                            eventData.TimeSlot_Inicio = !dr.IsDBNull(4) ? dr.GetTimeSpan(4) : default(TimeSpan);
+                            eventData.TimeSlot_Fim = !dr.IsDBNull(5) ? dr.GetTimeSpan(5) : default(TimeSpan);
+
+                            lst_disponibilidade.Add(eventData);
+                        }
+                    }
+                }
+            }
+
+            return lst_disponibilidade;
+        }
+    }
+}
